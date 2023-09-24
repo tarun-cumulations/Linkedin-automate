920feb84b2f85ef8f936a64619cb33b78b634999
Nirmal Kumar B <54202985+Nirmalj000@users.noreply.github.com>
2023-09-24 16:50:49 +0530

    Update scenario1.js
    added "for loop" of 5 to send connection request

--- scenario1.js
+++ scenario1.js
@@ -1,23 +1,20 @@
-let dotenv=require('dotenv');
+const dotenv = require('dotenv');
+dotenv.config();
 
-dotenv.config()
-
-let webdriver = require("selenium-webdriver");
-let By = webdriver.By;
-let until = webdriver.until;
-
+const webdriver = require("selenium-webdriver");
+const { elementTextMatches } = require('selenium-webdriver/lib/until');
+const By = webdriver.By;
+const until = webdriver.until;
 require("chromedriver");
 
-let driver = new webdriver.Builder()
+const driver = new webdriver.Builder()
     .forBrowser("chrome")
     .build();
 
 driver.get("https://in.linkedin.com/");
 
-
-let linkedin_username = process.env.LINKEDIN_USERNAME
-let linkedin_password = process.env.LINKEDIN_PASSWORD
-
+const linkedin_username = "tarun.t@cumulations.com";
+const linkedin_password = "blacy728";
 
 driver.wait(until.elementLocated(By.id("session_key")), 10000)
     .then(element => {
@@ -36,66 +33,58 @@ driver.wait(until.elementLocated(By.id("session_key")), 10000)
     .then(button => {
         return button.click();
     })
-    .then(()=>{
+    .then(() => {
         driver.get("https://www.linkedin.com/search/results/people/?industry=%5B%2296%22%2C%221594%22%2C%226%22%5D&keywords=people&network=%5B%22S%22%2C%22O%22%5D&origin=FACETED_SEARCH&sid=WPt");
-    }).then(()=>{
-        
-       // Full XPath for the "Connect" button
-        const fullXPath = '/html/body/div[4]/div[3]/div[2]/div/div[1]/main/div/div/div[3]/div/ul/li[3]/div/div/div[3]/div/button/span';
-
-        // Wait for the button to appear and click it
-        driver.wait(until.elementLocated(By.xpath(fullXPath)), 30000)
-            .then(element => {
-                return element.click();  // Click the button
-            })
-            .catch(err => {
-                console.log("Error: ", err);
-        });
-
     })
-    .then(()=>{
-        // add note
-
-        // Full XPath for the button you want to click
-        const fullXPath = '/html/body/div[3]/div/div/div[3]/button[1]/span';
-
-        // Wait for the button to appear and then click it
-        driver.wait(until.elementLocated(By.xpath(fullXPath)), 30000)
+    .then(() => {
+    // Loop for clicking the "Connect" button and adding a note 5 times
+    for (let i = 1; i <= 5; i++) {
+        const connectButtonXPath = `/html/body/div[4]/div[3]/div[2]/div/div[1]/main/div/div/div[2]/div/ul/li[${i}]/div/div/div[3]/div/button/span`;
+                                    
+        // Wait for the "Connect" button to appear and click it
+        driver.wait(until.elementLocated(By.xpath(connectButtonXPath)), 30000)
             .then(element => {
                 return element.click();  // Click the button
             })
             .catch(err => {
-                console.log("Error: ", err);
+                console.log("Error clicking 'Connect' button: ", err);
+            })
+            .then(() => {
+                // Full XPath for the button to add a note
+                const addNoteButtonXPath = '/html/body/div[3]/div/div/div[3]/button[1]/span';
+
+                driver.wait(until.elementLocated(By.xpath(addNoteButtonXPath)), 30000)
+                    .then(element => {
+                        return element.click();  // Click the "Add Note" button
+                    })
+                    .catch(err => {
+                        console.log("Error clicking 'Add Note' button: ", err);
+                    })
+                    .then(() => {
+                        // Full XPath for the textarea
+                        const textareaXPath = '/html/body/div[3]/div/div/div[2]/div/textarea';
+
+                        // Full XPath for the button to click after filling out the textarea
+                        const submitButtonXPath = '/html/body/div[3]/div/div/div[3]/button[2]/span';
+
+                        driver.wait(until.elementLocated(By.xpath(textareaXPath)), 30000)
+                            .then(element => {
+                                return element.sendKeys('Please connect');  // Send the note
+                            })
+                            .then(() => {
+                                return driver.wait(until.elementLocated(By.xpath(submitButtonXPath)), 30000);  // Wait for the button
+                            })
+                            .then(button => {
+                                return button.click();  // Click the button to submit the note
+                            })
+                            .catch(err => {
+                                console.log("Error submitting note: ", err);
+                            });
+                    });
             });
+    }
 
-            }).then(()=>{
-                // Full XPath for the textarea
-            // Full XPath for the textarea
-            const textareaXPath = '/html/body/div[3]/div/div/div[2]/div/textarea';
-
-            // Full XPath for the button to click after filling out the textarea
-            const buttonXPath = '/html/body/div[3]/div/div/div[3]/button[2]/span';
-
-            // Wait for the textarea, send the note, and then click the button
-            driver.wait(until.elementLocated(By.xpath(textareaXPath)), 30000)
-                .then(element => {
-                    return element.sendKeys('Please connect');  // Send the note
-                })
-                .then(() => {
-                    return driver.wait(until.elementLocated(By.xpath(buttonXPath)), 30000);  // Wait for the button
-                })
-                .then(button => {
-                    return button.click();  // Click the button
-                })
-                .catch(err => {
-                    console.log("Error: ", err);
-                });
-
-    })
-    .catch(err => {
-        console.log("Error: ", err);
-    });
-
-
-
-
+})
+.catch(err => {
+console.log("Error: ", err);
+});

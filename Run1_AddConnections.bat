872ac5957bde00819d4eeab288c70371bddaa66e
tarun-cumulations <tarun.t@cumulations.com>
2023-10-08 19:09:03 +0530

    Removing unneccessary files , adding bat files ,starting on scanrio3.js

---
+++ Run1_AddConnections.bat
@@ -0,0 +1,14 @@
+@echo off
+cd linkedin-automate
+
+REM Check if cd command was successful
+if %errorlevel% neq 0 (
+    echo Failed to change directory to 'linkedin-automate'. Please make sure the directory exists.
+    exit /b
+)
+
+REM Run the npm script
+npm run scenario1.js
+
+
+

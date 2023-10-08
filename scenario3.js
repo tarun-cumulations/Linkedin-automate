73faa0b745b5ee02e04bbfc3de292b724ca27514
tarun-cumulations <tarun.t@cumulations.com>
2023-10-08 19:23:25 +0530

    Adding login to scenario3

---
+++ scenario3.js
@@ -0,0 +1,41 @@
+let dotenv = require('dotenv');
+const winston = require('winston');
+const { Builder, By, until } = require('selenium-webdriver');
+
+dotenv.config();
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'application.log' })
+    ]
+});
+
+const linkedin_username = process.env.LINKEDIN_USERNAME;
+const linkedin_password = process.env.LINKEDIN_PASSWORD;
+const searchTitle = process.env.SEARCH_TITLE;
+
+async function salesNavigator() {
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    logger.info("EXECUTING SCENARIO 2 - MESSAGE THE 1st CONNECTIONS");
+
+    await driver.get("https://in.linkedin.com/");
+    logger.info("ON THE HOME SCREEN OF LINKEDIN");
+
+    let element = await driver.wait(until.elementLocated(By.id('session_key')), 10000);
+    await driver.executeScript(`arguments[0].value = "${linkedin_username}";`, element);
+
+    element = await driver.wait(until.elementLocated(By.id('session_password')), 10000);
+    await driver.executeScript(`arguments[0].value = "${linkedin_password}";`, element);
+
+    const complexSelector = "#main-content > section > div > div > form > div.flex.justify-between > button";
+    let button = await driver.wait(until.elementLocated(By.css(complexSelector)), 10000);
+
+    logger.info("SUCCESSFULLY LOGGED IN");
+    await button.click();
+}
+
+
+salesNavigator()
